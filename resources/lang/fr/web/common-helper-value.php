c3f476760196499a92c7801f8dd364e42450d1fa
gopalsaini <gopalsaini.img@gmail.com>
2023-01-12 14:51:52 +0530

    code push

---
+++ resources/lang/fr/web/common-helper-value.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Practitioner' => 'Practitioner',
+    'Facilitator' => 'Facilitator',
+    'Strategist' => 'Strategist',
+    'Partner' => 'Donor',
+    'Praticien' => 'Practitioner',
+    'Facilitateur' => 'Facilitator',
+    'Stratège' => 'Strategist',
+    'Partenaire' => 'Donor',
+    'Praticante' => 'Practitioner',
+    'Facilitador' => 'Facilitator',
+    'Estrategista/Estratega' => 'Strategist',
+    'Parceiro' => 'Donor',
+    'Practicante Especializado' => 'Practitioner',
+    'Facilitador' => 'Facilitator',
+    'Estratega' => 'Strategist',
+    'Socio' => 'Donor',
+    'N/A' => 'N/A',
+    'Mr.'=>'Mr.', // en
+    'Sr.'=> 'Mr.', // pt sp
+    'Ms.' => 'Ms.', //en
+    'Sra.'=> 'Ms.' , // sp
+    'Senhorita.' => 'Ms.' , // pt
+    'Mrs.'=> 'Mrs.', // en
+    'Srta.'=> 'Mrs.', // sp
+    'Senhora.'=> 'Mrs.' , // pt
+    'Dr.' => 'Dr.',
+    'Pasteur' => 'Pastor',
+    'Pastor' => 'Pastor', // sp
+    'Bishop' => 'Bishop', // en
+    'Bispo' => 'Bishop' , //pt
+    'Obispo' => 'Bishop', // sp
+    'Rev.' => 'Rev.',
+    'Prof.' => 'Prof.',
+
+];
